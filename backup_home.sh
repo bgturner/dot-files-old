37e1e4681373b32cafba0a57f9b912291671e34c
Benjamin Turner <benjamin@passionsplay.com>
2015-08-05 09:40:07 -0700

    Include .fonts folder with home backup

--- backup_home.sh
+++ backup_home.sh
@@ -17,7 +17,7 @@ tar cvzf $TEMPDIR/firefox-$NOW.tar.gz --exclude-caches-all --exclude-backups .mo
 tar cvzf $TEMPDIR/chromium-$NOW.tar.gz --exclude-caches-all --exclude-backups .config/chromium
 
 # Backing up home config
-tar cvzf $TEMPDIR/home-config-$NOW.tar.gz --exclude-caches-all --exclude-backups --exclude 'chromium/*' --exclude '.config/VirtualBox/*' --exclude '*/.git' --exclude '.local/share/Trash/*' .config .cinnamon .dot-files .filezilla .gnucash .icons .local .mypaint .ssh .themes .wallpaper .xmind .face
+tar cvzf $TEMPDIR/home-config-$NOW.tar.gz --exclude-caches-all --exclude-backups --exclude 'chromium/*' --exclude '.config/VirtualBox/*' --exclude '*/.git' --exclude '.local/share/Trash/*' .config .cinnamon .dot-files .filezilla .gnucash .icons .local .mypaint .ssh .themes .wallpaper .xmind .face .fonts
 
 # Backing up Vagrant, VirtualBox and VVV
 tar cvzf $TEMPDIR/vvv-$NOW.tar.gz --exclude-caches-all --exclude-backups --exclude 'VirtualBox\ VMs/Win8-50gb' --exclude 'node_modules' VirtualBox\ VMs vvv .config/VirtualBox .vagrant.d .config/VirtualBox
